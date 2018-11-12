db1ed427bc036d5e277f4be085e86d365c17d852
Dave Laird <kiwiupover@gmail.com>
2018-11-11 21:19:11 -0800

    scss

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  options: {
+    cssModules: {
+      extension: 'module.scss',
+      intermediateOutputPath: 'addon.scss',
+      postcssOptions: {
+        syntax: require('postcss-scss')
+      },
+      // generateScopedName: function(className, modulePath) {
+      //   return '_sass_addon' + generateScopedName(className, modulePath);
+      // }
+    }
+  }
 };
